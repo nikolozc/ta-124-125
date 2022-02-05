f5e2780228dd461efb000cdb51b91a58fb16f48f
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2022-02-05 12:29:13 -0500

    Finished 125
    Finished 125

--- Video_125/Form1.Designer.vb
+++ Video_125/Form1.Designer.vb
@@ -22,17 +22,42 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnLoad = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 25)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(276, 181)
+        Me.TextBox1.TabIndex = 0
+        '
+        'btnLoad
+        '
+        Me.btnLoad.Location = New System.Drawing.Point(20, 225)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(75, 23)
+        Me.btnLoad.TabIndex = 1
+        Me.btnLoad.Text = "Load File"
+        Me.btnLoad.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(419, 272)
+        Me.ClientSize = New System.Drawing.Size(308, 272)
+        Me.Controls.Add(Me.btnLoad)
+        Me.Controls.Add(Me.TextBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btnLoad As Button
 End Class
--- Video_125/Form1.vb
+++ Video_125/Form1.vb
@@ -1,3 +1,9 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Public Class Form1
 
+    Private fileToLoad As New StreamReader("D:\myFile.txt")
+
+    Private Sub btnLoad_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
+        TextBox1.Text = fileToLoad.ReadToEnd()
+    End Sub
 End Class
